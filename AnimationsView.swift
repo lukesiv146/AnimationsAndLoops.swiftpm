8cda515d388cdfc5a22e3636ba3306a33e8b564f
Luke Sivulka <lukesiv146@icloud.com>
2023-09-29 10:18:09 -0500

    Initial Commit

---
+++ AnimationsView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationsView.swift
+//  AnimationsAndLoops
+//
+//  Created by Luke J. Sivulka on 9/29/23.
+//
+
+import SwiftUI
+
+struct AnimationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationsView()
+    }
+}
---
+++ LoopsView.swift
@@ -0,0 +1,20 @@
+//
+//  LoopsView.swift
+//  AnimationsAndLoops
+//
+//  Created by Luke J. Sivulka on 9/29/23.
+//
+
+import SwiftUI
+
+struct LoopsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoopsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoopsView()
+    }
+}
